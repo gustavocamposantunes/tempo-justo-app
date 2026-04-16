9789b137c3eaa8b7733166106fbbb048bddf41af
decante <gustavocamposantunes@gmail.com>
2026-04-16 19:38:57 -0300

    chore: add storybook config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,66 +1,64 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
 import importPlugin from "eslint-plugin-import";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  {
-    plugins: {
-      import: importPlugin,
-    },
-    rules: {
-      "import/order": [
-        "warn",
-        {
-          groups: [
-            "builtin",   // node built-ins (fs, path)
-            "external",  // libs (react, next, zod)
-            "internal",  // aliases internos (@/...)
-            ["parent", "sibling", "index"], // relativos
-            "object",
-            "type",
-          ],
-          pathGroups: [
-            {
-              pattern: "react",
-              group: "external",
-              position: "before",
-            },
-            {
-              pattern: "next/**",
-              group: "external",
-              position: "before",
-            },
-            {
-              pattern: "@/**",
-              group: "internal",
-              position: "before",
-            },
-          ],
-          pathGroupsExcludedImportTypes: ["react"],
-          "newlines-between": "always",
-          alphabetize: {
-            order: "asc",
-            caseInsensitive: true,
-          },
-          warnOnUnassignedImports: true,
-        },
-      ],
-      "import/first": "warn",
-      "import/newline-after-import": "warn",
-      "import/no-duplicates": "warn",
-    },
+const eslintConfig = defineConfig([...nextVitals, ...nextTs, {
+  plugins: {
+    import: importPlugin,
   },
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+  rules: {
+    "import/order": [
+      "warn",
+      {
+        groups: [
+          "builtin",   // node built-ins (fs, path)
+          "external",  // libs (react, next, zod)
+          "internal",  // aliases internos (@/...)
+          ["parent", "sibling", "index"], // relativos
+          "object",
+          "type",
+        ],
+        pathGroups: [
+          {
+            pattern: "react",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "next/**",
+            group: "external",
+            position: "before",
+          },
+          {
+            pattern: "@/**",
+            group: "internal",
+            position: "before",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["react"],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+        warnOnUnassignedImports: true,
+      },
+    ],
+    "import/first": "warn",
+    "import/newline-after-import": "warn",
+    "import/no-duplicates": "warn",
+  },
+}, // Override default ignores of eslint-config-next.
+globalIgnores([
+  // Default ignores of eslint-config-next:
+  ".next/**",
+  "out/**",
+  "build/**",
+  "next-env.d.ts",
+]), ...storybook.configs["flat/recommended"]]);
 
 export default eslintConfig;
