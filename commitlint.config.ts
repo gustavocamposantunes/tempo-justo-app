e5a7f8f8e3d399599da2a2090889dcdc030ae77f
decante <gustavocamposantunes@gmail.com>
2026-04-20 09:11:47 -0300

    feat: increase header max length to 150 characters in commitlint configuration

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -4,7 +4,7 @@ const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
   formatter: '@commitlint/format',
   rules: {
-    'header-max-length': [2, 'always', 120],
+    'header-max-length': [2, 'always', 150],
   },
 }
 
